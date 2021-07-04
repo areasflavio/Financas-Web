500c6530ed7cc6b31a5b2bcd89ac88274e9831d2
Flávio Arêas <vflavio9@gmail.com>
2021-07-04 15:55:18 -0300

    feat: import file page

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Header from '../../components/Header';
+
+import alertImg from '../../assets/alert.svg';
+
+import { Container, Title, ImportFileContainer, Footer } from './styles';
 
 const Import: React.FC = () => {
-	return <h1>Import</h1>;
+	return (
+		<>
+			<Header />
+			<Container>
+				<Title>Importar uma transação</Title>
+				<ImportFileContainer>
+					<h1>Upload</h1>
+
+					<Footer>
+						<p>
+							<img src={alertImg} alt="Alert" />
+							Permitido apenas arquivos CSV
+						</p>
+						<button type="button">Enviar</button>
+					</Footer>
+				</ImportFileContainer>
+			</Container>
+		</>
+	);
 };
 
 export default Import;
---
+++ src/pages/Import/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.main`
+	width: 100%;
+	max-width: 46rem;
+	margin: 0 auto;
+	padding: 2.25rem 1.25rem;
+
+	margin-top: -7rem;
+	background: #fff;
+	border-radius: 0.25rem;
+`;
+
+export const Title = styled.h1`
+	font-weight: 500;
+	font-size: 2.25rem;
+	line-height: 3.375rem;
+	color: #365b80;
+	text-align: center;
+`;
+
+export const ImportFileContainer = styled.section`
+	margin-top: 2.5rem;
+	padding: 4rem;
+`;
+
+export const Footer = styled.section`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	margin-top: 2.25rem;
+
+	p {
+		display: flex;
+		align-items: center;
+
+		font-size: 0.75rem;
+		line-height: 1.125rem;
+		color: #365b80;
+
+		img {
+			margin-right: 0.3125rem;
+		}
+	}
+
+	button {
+		background: #ffab1f;
+		color: #fff;
+
+		padding: 1rem 5rem;
+		border: 0;
+		border-radius: 0.3125rem;
+
+		transition: background-color 0.2s;
+
+		&:hover {
+			background: ${shade(0.2, '#FFAB1F')};
+		}
+	}
+`;
