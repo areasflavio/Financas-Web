07bb9dcd529658b4d225b50ef51f7fd473b5e8e4
Flávio Arêas <vflavio9@gmail.com>
2021-07-06 20:47:48 -0300

    feat: import transactions files

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import filesize from 'filesize';
 
+import axios from 'axios';
 import Header from '../../components/Header';
 import Upload from '../../components/Upload';
+import FileList from '../../components/FileList';
+
+import api from '../../services/api';
 
 import alertImg from '../../assets/alert.svg';
 
 import { Container, Title, ImportFileContainer, Footer } from './styles';
 
+interface FileProps {
+	file: File;
+	name: string;
+	readableSize: string;
+}
+
 const Import: React.FC = () => {
+	const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
+	const history = useHistory();
+
+	async function handleUpload(): Promise<void> {
+		const arrayData: FormData[] = [];
+
+		uploadedFiles.forEach((uploadedFile) => {
+			const data = new FormData();
+
+			data.append('file', uploadedFile.file);
+
+			arrayData.push(data);
+		});
+
+		try {
+			const axiosData = arrayData.map((data) =>
+				api.post('/transactions/import', data),
+			);
+
+			await axios.all(axiosData);
+
+			history.push('/');
+		} catch (err) {
+			// eslint-disable-next-line no-console
+			console.error(err.response.error);
+		}
+	}
+
 	function submitFile(files: File[]): void {
-		// TODO
+		const formattedFiles = files.map((file) => ({
+			file,
+			name: file.name,
+			readableSize: filesize(file.size),
+		}));
+
+		setUploadedFiles(formattedFiles);
 	}
 
 	return (
@@ -19,13 +65,16 @@ const Import: React.FC = () => {
 				<Title>Importar uma transação</Title>
 				<ImportFileContainer>
 					<Upload onUpload={submitFile} />
+					{!!uploadedFiles.length && <FileList files={uploadedFiles} />}
 
 					<Footer>
 						<p>
 							<img src={alertImg} alt="Alert" />
 							Permitido apenas arquivos CSV
 						</p>
-						<button type="button">Enviar</button>
+						<button onClick={handleUpload} type="button">
+							Enviar
+						</button>
 					</Footer>
 				</ImportFileContainer>
 			</Container>
